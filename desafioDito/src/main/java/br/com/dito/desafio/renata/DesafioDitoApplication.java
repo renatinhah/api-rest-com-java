72408f70750728432844180f00f4de15a1a664b0
Renata <renata.ufop@yahoo.com.br>
2019-03-19 11:32:18 -0300

    criacao projeto spring boot vazio

---
+++ desafioDito/src/main/java/br/com/dito/desafio/renata/DesafioDitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dito.desafio.renata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDitoApplication.class, args);
+	}
+
+}
---
+++ desafioDito/src/test/java/br/com/dito/desafio/renata/DesafioDitoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.dito.desafio.renata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesafioDitoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
