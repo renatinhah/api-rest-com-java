919e91f11fc898ca0aa0dece90167d9c10042cf7
Renata <renata.ufop@yahoo.com.br>
2019-03-22 22:24:53 -0300

    Renomeando paths no endpoint

--- desafioDito/src/main/java/br/com/dito/desafio/renata/controller/EventoController.java
+++ desafioDito/src/main/java/br/com/dito/desafio/renata/controller/EventoController.java
@@ -7,16 +7,20 @@ import javax.validation.Valid;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.dito.desafio.renata.entity.Evento;
 import br.com.dito.desafio.renata.entity.Timeline;
 import br.com.dito.desafio.renata.service.EventoService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/evento")
 public class EventoController {
@@ -24,14 +28,14 @@ public class EventoController {
 	@Autowired
 	private EventoService eventoService;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public HashMap<String, ArrayList<Timeline>> getAllEventos() {
 		return eventoService.getAllEventos();
 	}
 	
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public ArrayList<Evento> getEventoById(@PathVariable("name") String name) {
-		return eventoService.getEventoByEvent(name);
+	@GetMapping
+	public ArrayList<Evento> getEventoById(@RequestParam("event") String event) {
+		return eventoService.getEventoByEvent(event);
 	}
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
